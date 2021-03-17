8ec6e1e45eeab9edca3f63c10a5d25e8c333fbac
Christopher Nethercott <ccnethercott@gmail.com>
2021-03-17 22:05:53 +0000

    Adding missing 'for'

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -3,7 +3,7 @@
 . /app/duck.conf
 RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$SUBDOMAINS&token=$TOKEN&ip="`
 if [ "$RESPONSE" = "OK" ]; then
-  echo "The IP(s) $SUBDOMAINS were updated at "$(date)
+  echo "The IP(s) for $SUBDOMAINS were updated at "$(date)
 else
   echo "Something went wrong, please check your settings  "$(date)
 fi
