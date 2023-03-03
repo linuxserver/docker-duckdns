0d7b541e88e55e8f7dffd0e8c373eeaba5822885
Eric Nemchik <eric@nemchik.com>
2023-03-02 21:09:46 -0600

    Rework shell scripts and cron logic

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/with-contenv bash
+# shellcheck shell=bash
 
-# shellcheck source=/dev/null
-. /app/duck.conf
-RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=")
-if [ "${RESPONSE}" = "OK" ]; then
-    echo "Your IP was updated at $(date)"
+if [ "${LOG_FILE}" = "true" ]; then
+    LOG_FILE="/config/duck.log"
+    touch "${LOG_FILE}"
+    /usr/sbin/logrotate /app/logrotate.conf
 else
-    echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
+    LOG_FILE="/dev/null"
 fi
+
+{
+    RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=")
+    if [ "${RESPONSE}" = "OK" ]; then
+        echo "Your IP was updated at $(date)"
+    else
+        echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
+    fi
+} | tee -a "${LOG_FILE}"
